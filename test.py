cac30a6c53274061b681a2b0116174ea3cdd1591
Rawtin <116755264+Rawtin@users.noreply.github.com>
2022-11-28 01:04:03 +0000

    canny test

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import cv2
 import numpy as np
 import pytesseract
+from scipy import ndimage
 
-img = cv2.imread('test.jpg')
+image = cv2.imread('test.jpg')
+cv2.imshow('OutputImage', image) #test
 
 h, w, c = img.shape
 boxes = pytesseract.image_to_boxes(img) 
@@ -10,5 +12,8 @@ for b in boxes.splitlines():
     b = b.split(' ')
     img = cv2.rectangle(img, (int(b[1]), h - int(b[2])), (int(b[3]), h - int(b[4])), (0, 255, 0), 2)
 
-cv2.imshow('img', img)
+cv2.imshow('image', img)
 cv2.waitKey(0)
+#grayscale picture needs to be applied#
+canny = cv2.Canny(img, 10, 155)
+cv2.imshow('canny image', canny)

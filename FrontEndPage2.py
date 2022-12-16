82ac4e1069e712245d0f62d05aa830f1bdf15281
root <root@pg.mfy5ukjmpgiu3ctpdmohwiwhke.zx.internal.cloudapp.net>
2022-12-16 11:51:35 +0000

    Changed the port

--- FrontEndPage2.py
+++ FrontEndPage2.py
@@ -48,5 +48,5 @@ def upload_image():
     return render_template('index2.html', form = form, file_url = file_url,file_output=file_output)
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    # app.run(host='0.0.0.0',port='8080')
+    #app.run(debug=True)
+    app.run(host='0.0.0.0',port='8080')

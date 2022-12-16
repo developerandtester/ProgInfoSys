45e219fb06f5106e3c3c1c694456cdc5f7e6b37b
abhi12manyu <abhi12manyu@gmail.com>
2022-12-16 01:17:02 +0000

    DisplayImageHTML

--- FrontEndPage.py
+++ FrontEndPage.py
@@ -1,5 +1,7 @@
 from flask import Flask, redirect, url_for,request, render_template,flash
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileAllowed
+from flask_uploads import UploadSet, IMAGES, configure_uploads
 from wtforms import FileField, SubmitField
 from werkzeug.utils import secure_filename
 import os
@@ -8,11 +10,19 @@ from wtforms.validators import InputRequired
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'PythonBoys'
 app.config['UPLOAD_FOLDER'] = 'UploadImages'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
+
+# ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 
 class UploadPicture(FlaskForm):
-    file = FileField("Picture", validators=[InputRequired()])
+    photo = FileField(
+        validators=[
+        InputRequired(),
+        FileAllowed(photos, 'Please upload a image'),
+        ]   )
     submit = SubmitField("Upload Picture")
 
 @app.route("/", methods = ['GET',"POST"])
@@ -21,17 +31,10 @@ def home():
     # return "<h1>Number Plate Recognition System<h1>"
     form=UploadPicture()
     if request.method == 'POST':
-        if 'file' not in request.files:
-            flash('No file part')
-            # return redirect(request.url)
-        #if form.validate_on_submit():
         file = request.files['file']
-        print(file)
-        if file:
-            filename = secure_filename(file.filename)
-            flash(file)
-            flash(app.config['UPLOAD_FOLDER'])
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        if photos:
+            filename = secure_filename(photos.filename)
+            photos.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             # return redirect(url_for('download_file', name=filename))
             #file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename)))
         # filename = secure_filename(file.filename)

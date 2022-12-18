0a63f006d84e3763b772ded39d7fc78a32aaa858
Prathamesh Vyas <vyasprathmesh@gmail.com>
2022-12-18 23:15:08 +0000

    Code Comments

--- FrontEndPage2.py
+++ FrontEndPage2.py
@@ -9,6 +9,8 @@ from wtforms.validators import InputRequired
 from paddleocr import PaddleOCR
 ocr = PaddleOCR(lang='en',rec_algorithm='CRNN')
 
+
+#initialising Flask
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'PythonBoys'
 app.config['UPLOADED_PHOTOS_DEST'] = 'UploadImages'
@@ -16,6 +18,7 @@ app.config['UPLOADED_PHOTOS_DEST'] = 'UploadImages'
 photos = UploadSet('photos', IMAGES)
 configure_uploads(app, photos)
 
+#Create a function for Validation on submit button
 class UploadForm(FlaskForm):
     photo=FileField(
         validators = [
@@ -24,29 +27,38 @@ class UploadForm(FlaskForm):
         ]
     )
     submit = SubmitField('Upload')
+    
 
+#Flask Route to send Uplooaded Image Path
 @app.route('/UploadImages/<filename>')
 def get_file(filename):
     return send_from_directory(app.config['UPLOADED_PHOTOS_DEST'],filename)
     
+    
+#Flask Route to index Page
 @app.route('/index2.html', methods= ['GET','POST'])
 def upload_image():
     form= UploadForm()
+    #Code for cliking on submit
     if form.validate_on_submit():
         filename = photos.save(form.photo.data)
         file_url = url_for('get_file', filename=filename)
+        #Getting Absolute Path from Relative Path 
         file_abs_path = os.path.join(app.config['UPLOADED_PHOTOS_DEST'], filename)
+        #Sending the image to get OCR output
         ocr_output=ocr.ocr(file_abs_path, cls=False, det=False)
+        #Finetuning the output for better readlibitliy
         res = ocr_output[0]
         PlateNo=''
         for line in res:
             PlateNo=line
         file_output ='The license plate number is '+PlateNo[0] + ' The accuracy of result is around ' + str((round(float(PlateNo[1]*100),2)))
-    else:
+    else:# First Time Page Loading
         file_url = None
         file_output = None
+    #Return Index2 Page
     return render_template('index2.html', form = form, file_url = file_url,file_output=file_output)
 
 if __name__ == "__main__":
     #app.run(debug=True)
-    app.run(host='0.0.0.0',port='8080')
+    app.run(host='0.0.0.0',port='8080')# To launch app from any IP configured and on Port 8080 reserved for flask on Azure
